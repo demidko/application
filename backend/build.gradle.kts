b9d69560632fc021467d68d4797fb97e7590c355
Daniel Demidko <demidko@farpost.com>
2023-07-08 11:21:04 +0600

    Обновление зависимостей

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -7,10 +7,10 @@ repositories {
   maven("https://repo.spring.io/snapshot")
 }
 plugins {
-  id("org.springframework.boot") version "3.1.1-SNAPSHOT"
+  id("org.springframework.boot") version "3.2.0-SNAPSHOT"
   id("io.spring.dependency-management") version "1.1.0"
-  kotlin("jvm") version "1.8.22"
-  kotlin("plugin.spring") version "1.8.22"
+  kotlin("jvm") version "1.9.0"
+  kotlin("plugin.spring") version "1.9.0"
 }
 dependencies {
   implementation("org.springframework.boot:spring-boot-starter-web")
