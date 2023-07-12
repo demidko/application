7ff9361e1a5eb37b8c6e457eb77ba16af25457a0
Daniel Demidko <demidko@farpost.com>
2023-07-13 05:26:47 +0600

    spring boot stable ver

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -7,7 +7,7 @@ repositories {
   maven("https://repo.spring.io/snapshot")
 }
 plugins {
-  id("org.springframework.boot") version "3.2.0-SNAPSHOT"
+  id("org.springframework.boot") version "3.1.1"
   id("io.spring.dependency-management") version "1.1.0"
   kotlin("jvm") version "1.9.0"
   kotlin("plugin.spring") version "1.9.0"
