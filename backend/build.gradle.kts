1cf02690c80f0cf530c8f948be0c3d4c5d50eac0
Daniil Demidko <demidko@farpost.com>
2024-01-20 01:48:48 +0600

    Kotlin 2.0.0-Beta3

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -9,8 +9,8 @@ repositories {
 plugins {
   id("org.springframework.boot") version "3.3.0-SNAPSHOT"
   id("io.spring.dependency-management") version "1.1.4"
-  kotlin("jvm") version "2.0.0-Beta2"
-  kotlin("plugin.spring") version "2.0.0-Beta2"
+  kotlin("jvm") version "2.0.0-Beta3"
+  kotlin("plugin.spring") version "2.0.0-Beta3"
 }
 dependencies {
   implementation("org.springframework.boot:spring-boot-starter-web")
